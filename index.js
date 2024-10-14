a4341ec8c82baac311fda5287be43171e7b35791
Cbaumgart004 <baumgart.c@gmail.com>
2024-10-14 14:47:33 -0600

    add role

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer = require('inquirer')
 const pool = require('./db') // Import the pool for database queries
 //import functions for each inquirer prompt from helper functions below
 const { viewEmployees, addEmployee } = require('./lib/employee')
-const { viewRoles } = require('./lib/role')
+const { viewRoles, addRole } = require('./lib/role')
 const { viewDepartments, addDepartment } = require('./lib/department')
 
 //function to start inquirer and handle user choices
@@ -17,6 +17,7 @@ const mainMenu = async () => {
         'View roles',
         'View departments',
         'Add employee',
+        'Add role',
         'Add department',
         'Update employee',
         'Delete employee',
@@ -40,6 +41,27 @@ const mainMenu = async () => {
 
     case 'Add employee':
       await addEmployee()
+      break
+
+    case 'Add role':
+      // departments for role creation
+      const departments = await pool.query('SELECT * FROM department')
+      const departmentChoices = departments.rows.map(({ id, name }) => ({
+        name,
+        value: id,
+      }))
+
+      const { title, salary, department_id } = await inquirer.prompt([
+        { name: 'title', message: 'Enter the role title:' },
+        { name: 'salary', message: 'Enter the salary for the role:' },
+        {
+          type: 'list',
+          name: 'department_id',
+          message: 'Choose a department for this role:',
+          choices: departmentChoices,
+        },
+      ])
+      await addRole(title, salary, department_id)
       break
 
     case 'Add department':
