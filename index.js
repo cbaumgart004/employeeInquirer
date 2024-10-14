b9843803e70e15d3118959847e70d09d3b0d73e4
Cbaumgart004 <baumgart.c@gmail.com>
2024-10-14 14:10:49 -0600

    added view roles and view departments.  Now we're cooking with gas

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const inquirer = require('inquirer')
 const pool = require('./db') // Import the pool for database queries
 //import functions for each inquirer prompt from helper functions below
 const { viewEmployees, addEmployee } = require('./lib/employee')
+const { viewRoles } = require('./lib/role')
+const { viewDepartments } = require('./lib/department')
 
 //function to start inquirer and handle user choices
 const mainMenu = async () => {
@@ -12,7 +14,10 @@ const mainMenu = async () => {
       message: 'Choose an action:',
       choices: [
         'View employees',
+        'View roles',
+        'View departments',
         'Add employee',
+
         'Update employee',
         'Delete employee',
         'Exit',
@@ -23,6 +28,14 @@ const mainMenu = async () => {
   switch (action) {
     case 'View employees':
       await viewEmployees()
+      break
+
+    case 'View roles':
+      await viewRoles()
+      break
+
+    case 'View departments':
+      await viewDepartments()
       break
 
     case 'Add employee':
