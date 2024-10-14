ae4255dc4bd673c4cf7a476021fc7bf3ffec1352
Cbaumgart004 <baumgart.c@gmail.com>
2024-10-14 16:03:55 -0600

    added update role function, and added block scoping to the cases

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const inquirer = require('inquirer')
 const pool = require('./db') // Import the pool for database queries
 //import functions for each inquirer prompt from helper functions below
-const { viewEmployees, addEmployee } = require('./lib/employee')
-const { viewRoles, addRole } = require('./lib/role')
+const { viewEmployees, addEmployee, updateEmployee } = require('./lib/employee')
+const { viewRoles, addRole, updateRole } = require('./lib/role')
 const { viewDepartments, addDepartment } = require('./lib/department')
 
-//function to start inquirer and handle user choices
+// Function to start inquirer and handle user choices
 const mainMenu = async () => {
   const { action } = await inquirer.prompt([
     {
@@ -19,13 +19,14 @@ const mainMenu = async () => {
         'Add employee',
         'Add role',
         'Add department',
-        'Update employee',
+        'Update employee role',
         'Delete employee',
         'Exit',
       ],
     },
   ])
-  //Switch case for each user choice
+
+  // Switch case for each user choice
   switch (action) {
     case 'View employees':
       await viewEmployees()
@@ -39,8 +40,8 @@ const mainMenu = async () => {
       await viewDepartments()
       break
 
-    case 'Add employee':
-      // Fetch roles from the database
+    case 'Add employee': {
+      // Use block scope to avoid variable re-declaration
       const roles = await pool.query('SELECT * FROM role')
       const roleChoices = roles.rows.map(({ id, title }) => ({
         name: title,
@@ -81,9 +82,10 @@ const mainMenu = async () => {
       // Pass the gathered data to the addEmployee function
       await addEmployee(first_name, last_name, role_id, manager_id)
       break
+    }
 
-    case 'Add role':
-      // departments for role creation
+    case 'Add role': {
+      // Use block scope to avoid variable re-declaration
       const departments = await pool.query('SELECT * FROM department')
       const departmentChoices = departments.rows.map(({ id, name }) => ({
         name,
@@ -102,19 +104,61 @@ const mainMenu = async () => {
       ])
       await addRole(title, salary, department_id)
       break
+    }
 
-    case 'Add department':
+    case 'Add department': {
+      // Use block scope to avoid variable re-declaration
       const { name } = await inquirer.prompt([
         { name: 'name', message: 'Enter the name of the new department:' },
       ])
       await addDepartment(name)
       break
+    }
+
+    case 'Update employee role': {
+      // Use block scope to avoid variable re-declaration
+      const roles = await pool.query('SELECT * FROM role')
+      const roleChoices = roles.rows.map(({ id, title }) => ({
+        name: title,
+        value: id,
+      }))
+
+      const employees = await pool.query('SELECT * FROM employee')
+      const employeeChoices = employees.rows.map(
+        ({ id, first_name, last_name }) => ({
+          name: `${first_name} ${last_name}`,
+          value: id,
+        })
+      )
+
+      // Inquirer prompt to select employee and role
+      const { employee_id, role_id } = await inquirer.prompt([
+        {
+          type: 'list',
+          name: 'employee_id',
+          message: 'Select an employee to update their role:',
+          choices: employeeChoices,
+        },
+        {
+          type: 'list',
+          name: 'role_id',
+          message: 'Select the new role for the employee:',
+          choices: roleChoices,
+        },
+      ])
+
+      // Pass the employee_id and role_id to the updateRole function
+      await updateRole(employee_id, role_id)
+      console.log(`Updated employee's role successfully.`)
+      break
+    }
 
     case 'Exit':
       console.log('Goodbye!')
       process.exit()
   }
-  //Show the main menu again after each action
+
+  // Show the main menu again after each action
   await mainMenu()
 }
 
